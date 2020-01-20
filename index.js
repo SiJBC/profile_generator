a71d7b5b3ef7d7a4cb01f3b1ae14687f959fc3a3
Simon Colman <s_colman@hotmail.com>
2020-01-20 14:04:58 +1100

    none

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const conversion = require("conversion");
+const pdf = require("html-pdf");
+const util = require("util");
+
+function promptUser() {
+    return inquirer.prompt([{
+        type: "input",
+        message: "Enter your Github username;",
+        name: "username"
+    }, {
+        type: " list",
+        message: " What's your favorite color?",
+        name: "faveColor",
+        choices: ['red', 'pink', 'green', 'blue']
+    }]);
+}
+promptUser()
+.then(function({username, faveColor}){
+    const queryUrl = `https://api.github.com/users/${username}`;
+    axios
+    .get(queryUrl).then(function(res){
+        info = {
+            color: faveColor,
+            profilePic: res.data.avatar_url,
+            name: res.data.login,
+            location: res.data.location,
+            profileUrl: res.data.html_url,
+            blog: res.data.blog,
+            bio: res.data.bio,
+            company: res.data.company,
+            repos: res.data.public_repos,
+            followers: res.data.followers,
+            following: res.data.following,
+          }
+          console.log(info);
+
+          const newQueryUrl = `https://api.github.com/users/${username}/repos`
+          console.log (newQueryUrl);
+
+          axios.get(newQueryUrl).then(function (res) {
+              let starCount = 0;
+              for (let index = 0; index<res.data.length; index ++){
+                  let count = res.data[index].stargazers_count;
+                  starCount = starCount + count;
+              }
+              console.log("Final star count for all repositories" + starCount)
+              info.starCount = starCount;
+         
+         
+            })
+        })
+    })
+
+
